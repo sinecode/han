fc9b253eaf4a3e535ee04589f171a8c7ed117ab7
ceccoemi <cecchini.mle@gmail.com>
2020-03-16 10:49:10 +0100

    Add Amazon in bow.py

--- bow.py
+++ bow.py
@@ -25,25 +25,31 @@ if not sys.warnoptions:
     os.environ["PYTHONWARNINGS"] = "ignore"  # Also affect subprocesses
 
 
-def train_test_split(df):
+def load_dataset(dataset_name):
+    return pd.read_csv(dataset_name)
+
+
+def train_test_split(df, train_size, test_size_per_class):
     """
     Split the dataset in text_train, y_train, text_test and y_test.
 
-    The training set is composed by 130,000 samples.
+    The training set is composed by `train_size` samples.
 
-    The test set is composed by 10,000 samples for each star,
-    there are a total of five stars so the test set has size 50,000.
+    The test set is composed by `test_size_per_class` samples for
+    each class.
     """
     test_df = pd.concat(
         [
-            df[df.stars == star].sample(10_000, random_state=20)
+            df[df.stars == star].sample(test_size_per_class, random_state=20)
             for star in df.stars.unique()
         ]
     )
-    assert (test_df.stars.value_counts(sort=False) == [10_000] * 5).all()
+    assert (
+        test_df.stars.value_counts(sort=False) == [test_size_per_class] * 5
+    ).all()
 
-    train_df = df.drop(test_df.index).sample(130_000, random_state=20)
-    assert (train_df.count() == [130_000] * 2).all()
+    train_df = df.drop(test_df.index).sample(train_size, random_state=20)
+    assert (train_df.count() == [train_size] * 2).all()
 
     text_train, y_train, text_test, y_test = (
         train_df.text.values,
@@ -66,8 +72,6 @@ def run_experiment(vectorizer, text_train, y_train, text_test, y_test):
     pipe = make_pipeline(
         vectorizer, MaxAbsScaler(copy=False), LogisticRegression(solver="sag"),
     )
-    # pipe.fit(text_train, y_train)
-    # return accuracy_score(y_test, pipe.predict(text_test))
     param_grid = {"logisticregression__C": [0.001, 0.01, 0.1, 1, 10]}
     grid = GridSearchCV(pipe, param_grid, cv=5, n_jobs=-1)
     grid.fit(text_train, y_train)
@@ -77,25 +81,33 @@ def run_experiment(vectorizer, text_train, y_train, text_test, y_test):
 
 
 def main():
-    df = pd.read_csv("datasets/yelp.csv")
-    assert (df.count() == [1_569_264] * 2).all()
-    text_train, y_train, text_test, y_test = train_test_split(df)
-    accuracy = run_experiment(
-        CountVectorizer(max_features=50_000),
-        text_train,
-        y_train,
-        text_test,
-        y_test,
-    )
-    print(f"BoW: {accuracy}")
-    accuracy = run_experiment(
-        TfidfVectorizer(max_features=50_000, norm=None),
-        text_train,
-        y_train,
-        text_test,
-        y_test,
-    )
-    print(f"BoW-TFIDF: {accuracy}")
+    exp_data = [
+        ("yelp.csv", 130_000, 10_000),
+        ("amazon.json", 600_000, 130_000),
+    ]
+    for dataset_name, train_size, test_size_per_class in exp_data:
+        print("==============================")
+        print(dataset_name.split(".")[0])
+        dataset = load_dataset(dataset_name)
+        text_train, y_train, text_test, y_test = train_test_split(
+            df, train_size, test_size_per_class
+        )
+        accuracy = run_experiment(
+            CountVectorizer(max_features=50_000),
+            text_train,
+            y_train,
+            text_test,
+            y_test,
+        )
+        print(f"BoW: {accuracy}")
+        accuracy = run_experiment(
+            TfidfVectorizer(max_features=50_000, norm=None),
+            text_train,
+            y_train,
+            text_test,
+            y_test,
+        )
+        print(f"BoW-TFIDF: {accuracy}")
 
 
 if __name__ == "__main__":
