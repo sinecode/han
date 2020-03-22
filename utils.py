b3de89c86016d23910c84bcb6236238225d3c926
ceccoemi <cecchini.mle@gmail.com>
2020-03-22 23:11:43 +0100

    Add some functions in utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from itertools import chain
+from collections import Counter
 from ast import literal_eval
 
 import pandas as pd
@@ -35,6 +36,18 @@ def flat_nested_list(x):
     [1, 2, 3]
     """
     return list(chain.from_iterable(x))
+
+
+def build_vocabulary(tokens, min_appearences=1):
+    """Return a vocabulary (set) of the tokenized text.
+
+    >>> sorted(build_vocabulary(['a', 'b', 'a', 'b', 'c']))
+    ['a', 'b', 'c']
+    >>> sorted(build_vocabulary(['a', 'b', 'a', 'b', 'c'], min_appearences=2))
+    ['a', 'b']
+    """
+    counter = Counter(tokens)
+    return set(token for token in counter if counter[token] >= min_appearences)
 
 
 if __name__ == "__main__":
