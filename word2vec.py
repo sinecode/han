cbf5dece2174540062c720cf3c8591ad6c1f4adb
ceccoemi <cecchini.mle@gmail.com>
2020-03-28 10:46:20 +0100

    Update word2vec.py

--- word2vec.py
+++ word2vec.py
@@ -2,43 +2,39 @@
 
 
 from statistics import mean
+from typing import List
+import sys
 
-import gensim
 from gensim.models import Word2Vec
 import pandas as pd
-import stanza
+from nltk.tokenize import sent_tokenize, word_tokenize
+from tqdm import tqdm
+
+
+def tokenize_text(text: str) -> List[List[str]]:
+    return [word_tokenize(sentence) for sentence in sent_tokenize(text)]
 
 
 class SentencesIterator:
-    def __init__(self, nlp_pipeline, text_df_column):
-        self.nlp_pipeline = nlp_pipeline
+    def __init__(self, text_df_column):
         self.text_column = text_df_column
         self.num_doc = len(text_df_column)
         self.num_sentences_per_doc = []
         self.num_words_per_doc = []
-        self.is_cache_active = False
-        self.cached_sentences = []
 
     def __iter__(self):
-        if not self.is_cache_active:
-            for text in self.text_column:
-                doc = self.nlp_pipeline(text)
-                self.num_sentences_per_doc.append(len(doc.sentences))
-                num_words = 0
-                for sentence in doc.sentences:
-                    tokens = [token.text.lower() for token in sentence.tokens]
-                    num_words += len(tokens)
-                    self.cached_sentences.append(tokens)
-                    yield tokens
-                self.num_words_per_doc.append(num_words)
-                if len(self.num_sentences_per_doc) % 1_000:
-                    print(
-                        f"Processed {len(self.num_sentences_per_doc)} of {self.num_doc} documents"
-                    )
-            self.is_cache_active = True
-        else:
-            for sentence in self.cached_sentences:
+        for text in tqdm(self.text_column):
+            sentences = tokenize_text(text.lower())
+            self.num_sentences_per_doc.append(len(sentences))
+            num_words = 0
+            for sentence in sentences:
+                num_words += len(sentence)
                 yield sentence
+            self.num_words_per_doc.append(num_words)
+
+    def reset_stats(self):
+        self.num_sentences_per_doc = []
+        self.num_words_per_doc = []
 
     def get_dataset_stats(self):
         return {
@@ -50,24 +46,25 @@ class SentencesIterator:
         }
 
 
-def train_word2vec_model(dataset_file: str) -> gensim.models.word2vec.Word2Vec:
+def train_word2vec_model(dataset_file: str):
     df = pd.read_csv(dataset_file).fillna("")
     model = Word2Vec(min_count=6, size=200)
-    nlp = stanza.Pipeline(
-        lang="en", processors="tokenize", use_gpu=True, verbose=False,
-    )
-    sentences_iter = SentencesIterator(nlp, df.text)
+    sentences_iter = SentencesIterator(df.text)
     model.build_vocab(sentences_iter)
-    print(model.corpus_count)
-    model.train(
-        sentences_iter, total_examples=model.corpus_count, epochs=model.epochs
-    )
     for stat_name, stat_value in sentences_iter.get_dataset_stats().items():
         print(f"{stat_name}: {stat_value}")
     print(f"Vocabulary size: {len(model.wv.vocab)}")
+    sentences_iter.reset_stats()
+    model.train(
+        sentences_iter, total_examples=model.corpus_count, epochs=model.epochs,
+    )
     return model
 
 
 if __name__ == "__main__":
-    model = train_word2vec_model("datasets/amazon.csv")
-    model.wv.save("embedding/amazon_embedding.wv")
+    dataset_file_name = sys.argv[1]
+    embedding_file_name = sys.argv[2]
+    print(f"Processing {dataset_file_name}")
+    model = train_word2vec_model(dataset_file_name)
+    model.wv.save(embedding_file_name)
+    print(f"Saved {embedding_file_name}")
