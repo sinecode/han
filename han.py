aa0fc0de1c0cda323135d418ede43e8e0aa19497
ceccoemi <cecchini.mle@gmail.com>
2020-04-05 14:57:53 +0200

    Add WordEncoder in han.py

--- han.py
+++ han.py
@@ -2,18 +2,21 @@ import torch
 
 
 class WordEncoder(torch.nn.Module):
-    def __init__(self, embedding_weights, hidden_size):
+    def __init__(self, embedding_matrix, hidden_size=50):
         super(WordEncoder, self).__init__()
+        embedding_dim = embedding_matrix.shape[1]
         self.embedding = torch.nn.Embedding.from_pretrained(
-            embedding_weights, freeze=True
+            embeddings=torch.FloatTensor(embedding_matrix), freeze=True,
         )
         self.gru = torch.nn.GRU(
-            input_size=self.embedding.embedding_dim,
+            input_size=embedding_dim,
             hidden_size=hidden_size,
             bidirectional=True,
         )
+        self.word_context_vector = torch.nn.Parameter(
+            torch.Tensor(2 * hidden_size, 2 * hidden_size)
+        )
 
-    def forward(self, input, hidden):
-        embedding = self.embedding(input).view(1, 1, -1)
-        output, hidden = self.gru(embedding, hidden)
-        return output, hidden
+    def forward(self, input, hidden_state):
+        output = self.embedding(input)
+        return self.gru(output.float(), hidden_state)
