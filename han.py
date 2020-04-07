600367399dd37f0721771ee70e1c62d061864a95
ceccoemi <cecchini.mle@gmail.com>
2020-04-07 11:53:55 +0200

    Maybe completed han

--- han.py
+++ han.py
@@ -1,25 +1,83 @@
 import torch
 
 
-class WordEncoder(torch.nn.Module):
-    """
-    Two layers:
-        - embedding (pretrained)
-        - bidirectional GRU
-    """
+class Encoder(torch.nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(Encoder, self).__init__()
+        self.gru = torch.nn.GRU(
+            input_size=input_size, hidden_size=hidden_size, bidirectional=True,
+        )
 
-    def __init__(self, embedding_matrix, hidden_size=50):
-        super(WordEncoder, self).__init__()
+    def forward(self, input, hidden_state):
+        return self.gru(input, hidden_state)
+
+
+class Attention(torch.nn.Module):
+    def __init__(self, input_size):
+        super(Attention, self).__init__()
+        self.context_vector = torch.nn.Parameter(torch.Tensor(input_size, 1))
+        self.linear = torch.nn.Linear(
+            in_features=input_size, out_features=input_size
+        )
+
+    def forward(self, input):
+        output = torch.tanh(self.linear(input))
+        output = torch.matmul(output, self.context_vector).squeeze()
+        output = torch.nn.functional.softmax(output, dim=-1)
+        return sum(
+            alpha.unsqueeze(1).expand_as(h) * h
+            for alpha, h in zip(output, input)
+        )
+
+
+class Han(torch.nn.Module):
+    def __init__(
+        self,
+        embedding_matrix,
+        batch_size=64,
+        word_hidden_size=50,
+        sent_hidden_size=50,
+    ):
+        super(Han, self).__init__()
         embedding_dim = embedding_matrix.shape[1]
         self.embedding = torch.nn.Embedding.from_pretrained(
             embeddings=torch.FloatTensor(embedding_matrix), freeze=True,
         )
-        self.gru = torch.nn.GRU(
-            input_size=embedding_dim,
-            hidden_size=hidden_size,
-            bidirectional=True,
+        self.word_hidden_state = torch.zeros(2, batch_size, word_hidden_size)
+        self.word_encoder = Encoder(embedding_dim, word_hidden_size)
+        self.word_attention = Attention(input_size=word_hidden_size * 2)
+        self.sent_hidden_state = torch.zeros(2, batch_size, sent_hidden_size)
+        self.sent_encoder = Encoder(word_hidden_size * 2, sent_hidden_size)
+        self.sent_attention = Attention(input_size=sent_hidden_size * 2)
+        self.last_layer = torch.nn.Linear(
+            in_features=sent_hidden_size * 2, out_features=1
         )
 
-    def forward(self, input, hidden_state):
-        output = self.embedding(input)
-        return self.gru(output.float(), hidden_state)
+    def forward(self, input):
+        output_list = []
+        # reshape as [num_sent_per_doc, batch_size, num_words_per_doc]
+        input = input.permute(1, 0, 2)
+        # for each sentence in input..
+        #
+        # i is all the first sentences for each doc,
+        # then i is all the sencond sentences for each doc,
+        # then i is all the third sentences for each doc,
+        # etc...
+        for i in input:
+            # reshape as [num_words_per_doc, batch_size]
+            i = i.permute(1, 0)
+            output = self.embedding(i)
+            output, self.word_hidden_state = self.word_encoder(
+                output.float(), self.word_hidden_state
+            )
+            output = self.word_attention(output).unsqueeze(0)
+            output_list.append(output)
+        output = torch.cat(output_list, 0)
+        output, self.sent_hidden_state = self.sent_encoder(
+            output, self.sent_hidden_state
+        )
+        output = self.sent_attention(output)
+        output = torch.nn.functional.softmax(
+            self.last_layer(output), dim=-1
+        ).squeeze(1)
+        return output
