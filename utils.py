3a011d500d47cd04025db9535288c1ed2b742311
ceccoemi <cecchini.mle@gmail.com>
2020-04-13 11:11:50 +0200

    Removed tokenization

--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-import pickle
 from typing import List
 
 from nltk.tokenize import sent_tokenize, word_tokenize
@@ -14,17 +13,6 @@ def tokenize_doc(doc: str) -> List[List[str]]:
     """
     doc = doc.lower().strip()
     return [word_tokenize(sentence) for sentence in sent_tokenize(doc)]
-
-
-def save_obj_as_pickle(obj, filename):
-    with open(filename, "wb") as f:
-        pickle.dump(obj, f)
-
-
-def load_pickled_obj(filename):
-    with open(filename, "rb") as f:
-        obj = pickle.load(f)
-    return obj
 
 
 if __name__ == "__main__":
